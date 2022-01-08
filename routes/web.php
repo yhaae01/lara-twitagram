940cedf439d264cc21803dc34f2a83c97d412f50
yhaae01 <suryaintpermana@gmail.com>
2022-01-09 05:56:43 +0700

    Membuat controller dan model Status

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TimelineController;
 
 Route::view('/', 'welcome');
-Route::get('timeline', TimelineController::class)->name('timeline');
 
-Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function (){
+   Route::get('timeline', TimelineController::class)->name('timeline');
+   Route::view('/dashboard', 'dashboard')->name('dashboard');
+});
+
 
 require __DIR__.'/auth.php';
