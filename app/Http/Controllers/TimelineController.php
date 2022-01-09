a7b670038de7e3e243442fb415e1f08e99b04a78
yhaae01 <suryaintpermana@gmail.com>
2022-01-09 07:56:26 +0700

    Menampilkan status sendiri dan orang yang di follow

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -10,7 +10,7 @@ class TimelineController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $statuses = Status::where('user_id', Auth::user()->id)->get();
+        $statuses = Auth::user()->timeline();
         return view('timeline', compact('statuses'));
     }
 }
