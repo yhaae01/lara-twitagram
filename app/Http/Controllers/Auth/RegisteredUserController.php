0d543f1917fe3d3865b4d599488efc9dc4f45ed9
yhaae01 <suryaintpermana@gmail.com>
2022-01-09 09:50:15 +0700

    Memperbaiki DatabaseSeeder dan RegisterController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
-            'username' => strtolower(Str::random(10)),
+            'username' => 'u' . strtolower(Str::random(14)),
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
