c23d5ec3ebc2ca15edbcf35eb9b47810a130be24
yhaae01 <suryaintpermana@gmail.com>
2022-01-09 08:13:07 +0700

    Redesign Timeline dan Menambahkan form status

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,3 @@
+<div class="border border-gray-400 p-5 rounded-lg">
+{{ $slot }}
+</div>
--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -2,33 +2,52 @@
    <x-container>
       <div class="grid grid-cols-12 gap-6">
          <div class="col-span-8">
-            <h1 class="font-semibold mb-4">All Status</h1>
-            <div class="space-y-6">
-               <div class="border rounded-xl p-5 space-y-5">
-                  @foreach ($statuses as $status)
-                     <div class="flex">
-                        <div class="flex-shrink-0 mr-3">
-                           <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="{{ $status->user->name }}">
+            <x-card>
+               <form action="" method="post">
+                  <div class="flex">
+                     <div class="flex-shrink-0 mr-3">
+                        <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="{{ Auth::user()->name }}">
+                     </div>
+                     <div class="w-full">
+                        <div class="leading-relaxed">
+                           <textarea name="body" id="body" placeholder="What is your mind ?" rows="5" class="form-textarea w-full border-gray-300 rounded-lg resize-none focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"></textarea>
                         </div>
-                        <div>
-                           <div class="font-semibold">
-                              {{ $status->user->name }}
-                           </div>
-                           <div class="leading-relaxed">
-                              {{ $status->body }}
-                           </div>
-                           <div class="text-gray-500 text-sm">
-                              {{ $status->created_at->diffForHumans() }}
-                           </div>
+                        <div class="mt-2 text-right">
+                           <x-button>Post</x-button>
                         </div>
                      </div>
+                  </div>
+               </form>
+            </x-card>
+            <h1 class="font-semibold mb-4 mt-4">All Status</h1>
+            <div class="space-y-6 mb-4">
+               <div class="space-y-5">
+                  @foreach ($statuses as $status)
+                     <x-card>
+                        <div class="flex">
+                           <div class="flex-shrink-0 mr-3">
+                              <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="{{ $status->user->name }}">
+                           </div>
+                           <div>
+                              <div class="font-semibold">
+                                 {{ $status->user->name }}
+                              </div>
+                              <div class="leading-relaxed">
+                                 {{ $status->body }}
+                              </div>
+                              <div class="text-gray-500 text-sm">
+                                 {{ $status->created_at->diffForHumans() }}
+                              </div>
+                           </div>
+                        </div>
+                     </x-card>
                   @endforeach
                </div>
             </div>
          </div>
          <div class="col-span-4">
             <h1 class="font-semibold mb-4">Recent Follow</h1>
-            <div class="border p-5 rounded-xl">
+            <x-card>
                <div class="space-y-5">
                   @foreach (Auth::user()->follows()->limit(5)->get() as $user)
                      <div class="flex items-center">
@@ -46,7 +65,7 @@
                      </div>
                   @endforeach
                </div>
-            </div>
+            </x-card>
          </div>
       </div>
    </x-container>
